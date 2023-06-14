f29946aa3117fd8c475c9a8ea54dbc3bce02d3bd
mishkajackson <92293134+mishkajackson@users.noreply.github.com>
2023-06-14 23:57:49 +0300

    calendar

--- front/src/components/UI/CardMe/Dot.jsx
+++ front/src/components/UI/CardMe/Dot.jsx
@@ -1,11 +1,31 @@
+import moment from "moment";
 import styles from "./style.module.css";
 
-function Dot({date}) {
+function Dot({ items, date }) {
+  const filteredItems = items.filter(
+    (item) =>
+      moment(item.date).format("DD.MM.YYYY") === date.format("DD.MM.YYYY")
+  );
 
-
-    return (
-        <div style={{backgroundColor: '#6f7fff'}} className={styles.dot}></div>
-    )
+  return (
+    <div className={styles.dots}>
+      {filteredItems.map((item) => (
+        <div
+          key={item.id}
+          style={
+            item.cabId === 3
+              ? { backgroundColor: "#6f7fff" }
+              : item.cabId === 4
+              ? { backgroundColor: "#00a600" }
+              : item.cabId === 5
+              ? { backgroundColor: "#ff5b5b" }
+              : ""
+          }
+          className={styles.dot}
+        ></div>
+      ))}
+    </div>
+  );
 }
 
-export default Dot
+export default Dot;
