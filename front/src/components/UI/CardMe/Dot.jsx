c820273dbf6a3127368a178b971374277c070f41
mishkajackson <92293134+mishkajackson@users.noreply.github.com>
2023-06-16 12:00:57 +0300

    Revert "calendar"
    This reverts commit f29946aa3117fd8c475c9a8ea54dbc3bce02d3bd.

--- front/src/components/UI/CardMe/Dot.jsx
+++ front/src/components/UI/CardMe/Dot.jsx
@@ -1,31 +1,11 @@
-import moment from "moment";
 import styles from "./style.module.css";
 
-function Dot({ items, date }) {
-  const filteredItems = items.filter(
-    (item) =>
-      moment(item.date).format("DD.MM.YYYY") === date.format("DD.MM.YYYY")
-  );
+function Dot({date}) {
 
-  return (
-    <div className={styles.dots}>
-      {filteredItems.map((item) => (
-        <div
-          key={item.id}
-          style={
-            item.cabId === 3
-              ? { backgroundColor: "#6f7fff" }
-              : item.cabId === 4
-              ? { backgroundColor: "#00a600" }
-              : item.cabId === 5
-              ? { backgroundColor: "#ff5b5b" }
-              : ""
-          }
-          className={styles.dot}
-        ></div>
-      ))}
-    </div>
-  );
+
+    return (
+        <div style={{backgroundColor: '#6f7fff'}} className={styles.dot}></div>
+    )
 }
 
-export default Dot;
+export default Dot
