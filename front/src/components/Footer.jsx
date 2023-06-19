b42a1cc8d2f2cd07c495be08ca299f0a22c743af
mishkajackson <92293134+mishkajackson@users.noreply.github.com>
2023-06-19 21:26:38 +0300

    delete main swiper

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -7,31 +7,37 @@ import '../App.css';
 
 
 
-function Footer({slide}) {
+function Footer() {
     const navButtons = [
-        { name: 'Сегодня', icon: faCalendarCheck, active: true, index: 0 },
-        { name: 'Смены', icon: faStethoscope, active: false, index: 1 },
-        { name: 'Графики', icon: faCalendarDays, active: false, index: 2 },
-        { name: 'Профиль', icon: faUser, active: false, index: 3 }
+        { name: 'Сегодня', icon: faCalendarCheck, active: true, link: '/' },
+        { name: 'Смены', icon: faStethoscope, active: false, link: '/Me' },
+        { name: 'Графики', icon: faCalendarDays, active: false, link: '/Schedule' },
+        { name: 'Профиль', icon: faUser, active: false, link: '/User' }
         
     ]
     return (
-      <div>
-        <Outlet />
-        <div className="footer">
-          <div className="navButtons">
-            {navButtons.map((navButton) => (
-              <div key={navButton.index} onClick={() => slide(navButton.index)}>
-                <button className="navButton">
-                  <FontAwesomeIcon className="navIcon" icon={navButton.icon} />
-                  <span className="navText">{navButton.name}</span>
-                </button>
-              </div>
-            ))}
-          </div>
+        
+       
+            <div>
+                <Outlet />
+            <div className='footer'>
+                <div className='navButtons'>
+                    {
+                        navButtons.map((navButton) => (
+                            <NavLink key={navButton.link} to={navButton.link}>
+                                    <button className='navButton' >
+                                        <FontAwesomeIcon className='navIcon' icon={navButton.icon} />
+                                        <span className='navText'>{navButton.name}</span>
+                                    </button>
+                                </NavLink>
+                        ))
+                    }
+                </div>
+            </div>
+            
+            
         </div>
-      </div>
-    );
+    )
 }
 
 export default Footer
--- front/src/components/MainPage.jsx
+++ front/src/components/MainPage.jsx
@@ -1,51 +1,19 @@
 
 import Footer from '../components/Footer';
-import Today from './Views/Today/Today';
-import Me from "./Views/Me/Me";
-import Schedule from "./Views/Schedule/Schedule";
-import User from "./Views/User/User";
 import { Navigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
 
 const MainPage = () => {
     const authLocalStorage = JSON.parse(localStorage.getItem("authenticated"))
     const [authenticated, setAuthenticated] = useState(authLocalStorage);
-     const [swipe, setSwipe] = useState(0);
-     const [tab, setTab] = useState(0);
-
-     function slideTo(page) {
-        swipe.slideTo(page);
-     }
 
     if (!authenticated) {
         return <Navigate replace to="/Login" />;
     } else {
         return (
-          <div>
-            <Swiper
-              spaceBetween={100}
-              slidesPerView={1}
-              
-              onBeforeInit={(swipper) => setSwipe(swipper)}
-            >
-              <SwiperSlide>
-                <Today />
-              </SwiperSlide>
-              <SwiperSlide>
-                <Me />
-              </SwiperSlide>
-              <SwiperSlide>
-                <Schedule />
-              </SwiperSlide>
-              <SwiperSlide>
-                <User />
-              </SwiperSlide>
-            </Swiper>
-
-            <Footer slide={slideTo}></Footer>
-          </div>
+            <div>
+                <Footer></Footer>
+            </div>
         );
     }
 };
